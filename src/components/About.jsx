e5ddb6b21fc0eb3b63779d74a8caf8384d4355ae
Matheus Cavasini <cavasini.dev@gmail.com>
2023-09-16 02:37:43 -0300

    mudando textos do about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,22 @@
 import {} from "react";
 
-import mockupHome from '/mockupHome.svg'
-import mockupNotficacoes from '/mockupNotifica.svg'
-import mockupFavoritos from '/mockupFav.svg'
+import mockupHome from "/mockupHome.svg";
+import mockupNotficacoes from "/mockupNotifica.svg";
+import mockupFavoritos from "/mockupFav.svg";
 
-import * as S from './styles'
+import * as S from "./styles";
 
 const About = () => {
   return (
     <section className="grid-container">
       <div className="section">
-          <img src={mockupHome} alt=""/>
+        <img src={mockupHome} alt="" />
         <div className="section-content">
-          <h3>Conectividade Ecológica</h3>
+          <h3> Página Inicial Sustentável</h3>
           <p>
-            Descubra como nossa tecnologia Bluetooth Low Energy está conectando
-            sensores ambientais para coletar dados precisos e preservar
-            ecossistemas valiosos em todo o mundo.
+            A página inicial do aplicativo é o ponto de partida para sua jornada
+            de sustentabilidade, oferecendo informações e recursos para inspirar
+            ações ecológicas.
           </p>
           <div className="downloadButtom_div">
             <a>Download Now</a>
@@ -24,13 +24,13 @@ const About = () => {
         </div>
       </div>
       <div className="section">
-          <img src={mockupNotficacoes} alt=""/>
+        <img src={mockupNotficacoes} alt="" />
         <div className="section-content">
-          <h3>Eficiência Energética Avançada</h3>
+          <h3>Notificações Eco-Amigáveis</h3>
           <p>
-            Saiba como nosso projeto está impulsionando a eficiência energética,
-            reduzindo o consumo de energia em dispositivos de monitoramento
-            ambiental e ajudando a conservar recursos naturais.
+            Mantenha-se atualizado com notificações sobre eventos sustentáveis e
+            descontos em produtos eco-friendly, tudo personalizado com base em
+            seus interesses e preferências.
           </p>
           <div className="downloadButtom_div">
             <a>Download Now</a>
@@ -38,13 +38,13 @@ const About = () => {
         </div>
       </div>
       <div className="section">
-          <img src={mockupFavoritos} alt=""/>
+        <img src={mockupFavoritos} alt="" />
         <div className="section-content">
-          <h3>A Revolução Sustentável</h3>
+          <h3>Seus Favoritos Verdes</h3>
           <p>
-            Explore como nossa iniciativa está liderando a revolução
-            sustentável, unindo ecologia e tecnologia Bluetooth Low Energy para
-            proteger o planeta e construir um futuro mais verde.
+            Crie e gerencie sua lista de produtos e lugares favoritos,
+            promovendo um estilo de vida sustentável com facilidade. Personalize
+            sua lista de acordo com suas escolhas ecológicas.
           </p>
           <div className="downloadButtom_div">
             <a>Download Now</a>
