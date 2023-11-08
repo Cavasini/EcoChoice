4880c0a9edce296fb4f876bc193decb376879384
cavasini <cavasini.dev@gmail.com>
2023-11-08 20:18:20 -0300

    register

--- src/components/Form/ClienteForm.jsx
+++ src/components/Form/ClienteForm.jsx
@@ -1,74 +1,115 @@
-// import  { useState, useEffect } from "react";
-// import {Input, Button } from "../styles";
+import {} from "react";
+import "./form.css"
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-// const generateUniqueKey = () => {
-//   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   const numbers = "0123456789";
+const schema = yup
+  .object({
+    name: yup.string().required(),
+    email: yup
+      .string()
+      .email("Digite um email valido")
+      .required("O Campo email obrigatório"),
+    cel: yup
+      .string()
+      .matches(/^\(\d{2}\) 9\d{4}-\d{4}$/, "O número de celular não é válido")
+      .required("O número de celular é obrigatório"),
+  })
+  .required();
 
-//   let key = "";
+function Form() {
+  let { id } = useParams();
 
-//   for (let i = 0; i < 3; i++) {
-//     const randomIndex = Math.floor(Math.random() * letters.length);
-//     key += letters[randomIndex];
-//   }
+  const [client, setClient] = useState({
+    id,
+    name: "",
+    email: "",
+    cel: "",
+  });
 
-//   for (let i = 0; i < 3; i++) {
-//     const randomIndex = Math.floor(Math.random() * numbers.length);
-//     key += numbers[randomIndex];
-//   }
+  let metodo = "post";
+  if (id) {
+    metodo = "put";
+  }
 
-//   return key;
-// };
+  const handleChange = (e) => {
+    setClient({ ...client, [e.target.name]: e.target.value });
+  };
 
-// const ClienteForm = () => {
-//   const [formData, setFormData] = useState({});
-//   const [uniqueKey, setUniqueKey] = useState("");
+  const OnSubmit = () => {
+    fetch(`http://localhost:5000/clients/${id ? id : ""}`, {
+      method: metodo,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(client),
+    }).then(() => {
+      window.location = "/";
+    });
+  };
 
-//   useEffect(() => {
-//     const savedData = JSON.parse(localStorage.getItem(uniqueKey)) || {};
-//     setFormData(savedData);
-//   }, [uniqueKey]);
+  useEffect(() => {
+    if (id) {
+      fetch(`http://localhost:5000/clients/${id}`)
+        .then((resp) => {
+          return resp.json();
+        })
+        .then((data) => {
+          setClient(data);
+        });
+    }
+  }, [id]);
 
-//   const handleSubmit = () => {
-//     const newKey = generateUniqueKey();
-//     setUniqueKey(newKey);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-//     const dataToSave = {
-//       name: formData.name,
-//       email: formData.email,
-//     };
+  return (
+    <>
+      <form onSubmit={handleSubmit(OnSubmit)}>
+        <label type="name">Nome Completo:</label>
+        <input
+          {...register("name")}
+          type="text"
+          name="name"
+          value={client.name}
+          placeholder="Digite seu nome"
+          onChange={handleChange}
+        />
+        <span className="text-danger">{errors.name?.message}</span>
 
-//     localStorage.setItem(newKey, JSON.stringify(dataToSave));
-//     sessionStorage.setItem(newKey, JSON.stringify(dataToSave));
+        <label type="email">Email:</label>
+        <input
+          {...register("email")}
+          type="text"
+          name="email"
+          value={client.idade}
+          placeholder="Digite sua idade"
+          onChange={handleChange}
+        />
+        <span>{errors.email?.message}</span>
 
-//     alert(`Seja bem vindo ${formData.name}`);
-//   };
-
-//   return (
-//     <form action="#">
-//       <div className="form-group">
-//         <Input
-//           placeholder="Name"
-//           type="text"
-//           name="name"
-//           value={formData.name || ""}
-//           onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-//         />
-//       </div>
-//       <div className="form-group">
-//         <Input
-//           placeholder="Email"
-//           type="text"
-//           name="email"
-//           value={formData.email || ""}
-//           onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-//         />
-//       </div>
-//       <Button type="submit" onClick={handleSubmit}>
-//         Enviar
-//       </Button>
-//     </form>
-//   );
-// };
-
-// export default ClienteForm;
+        <label type="celular">Celular:</label>
+        <input
+          {...register("cel")}
+          id="phone"
+          type="tel"
+          name="cel"
+          value={client.cel}
+          placeholder="Digite seu numero de celular"
+          onChange={handleChange}
+        />
+        <span>{errors.cel?.message}</span>
+        <input type="submit" value="Cadastrar" />
+      </form>
+    </>
+  );
+}
+export default Form;
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,77 +1,14 @@
 import {} from "react";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import Form from "../components/Form/ClienteForm";
 
 function Register() {
-  let { id } = useParams();
-
-  const [client, setClient] = useState({
-    id,
-    name: "",
-    cel: "",
-  });
-
-  let metodo = "post";
-  if (id) {
-    metodo = "put";
-  }
-
-  const handleChange = (e) => {
-    setClient({ ...client, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch(`http://localhost:5000/clients/${id ? id : ""}`, {
-      method: metodo,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(client),
-    }).then(() => {
-      window.location = "/";
-    });
-  };
-
-  useEffect(() => {
-    if (id) {
-      fetch(`http://localhost:5000/clients/${id}`)
-        .then((resp) => {
-          return resp.json();
-        })
-        .then((data) => {
-          setClient(data);
-        });
-    }
-  }, [id]);
+  
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label type="nome">Nome Completo:</label>
-        <input
-          type="text"
-          name="name"
-          value={client.nome}
-          placeholder="Digite seu nome"
-          onChange={handleChange}
-          required
-        />
-
-        <label type="email">Email:</label>
-        <input
-          type="text"
-          name="email"
-          value={client.idade}
-          placeholder="Digite sua idade"
-          onChange={handleChange}
-        />
-
-        <label type="celular">Celular:</label>
-        <input type="tel" name="cel" value={client.cel} placeholder="Digite seu numero de celular" onChange={handleChange} required/>
-
-        <input type="submit" value="Cadastrar" />
-      </form>
+    <div className="container ">
+      <Form/>
+    </div>
     </>
   );
 }
